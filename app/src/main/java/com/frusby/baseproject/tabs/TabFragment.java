b4396cab659051f5f6afcff03004fbdcfc18e272
AlexisEveryStory <alexis.jouhault@gmail.com>
2016-04-29 19:29:29 -0700

    first commit

---
+++ app/src/main/java/com/frusby/baseproject/tabs/TabFragment.java
@@ -0,0 +1,26 @@
+package com.frusby.baseproject.tabs;
+
+import android.content.Context;
+
+import com.frusby.baseproject.BaseComponents.BaseFragment;
+import com.frusby.baseproject.BaseComponents.BaseFragmentActivity;
+import com.frusby.baseproject.MainActivity;
+
+/**
+ * Created by alexisjouhault on 4/24/16.
+ */
+public class TabFragment extends BaseFragment {
+
+    protected BaseFragmentActivity parentActivity;
+    protected String title;
+
+    @Override
+    public void onAttach(Context context) {
+        this.parentActivity = (BaseFragmentActivity) context;
+        super.onAttach(context);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
